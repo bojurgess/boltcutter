04a9c05cf2d036c380c401a6e66ceee876891ba8
Joe Burgess <bojurgess@gmail.com>
2024-11-06 09:28:06 +0000

    whoops

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,13 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>boltcutter.</title>
+	<meta name="description" content="A URL shortening service" />
+	<meta name="keywords" content="url, shortener, link, shortening" />
+	<meta name="author" content="Boltcutter" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
 <Toaster position="top-center" richColors />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,18 +23,16 @@
 	let url = $derived(urlFromID(form?.id));
 </script>
 
-<svelte:head>
-	<title>boltcutter.</title>
-</svelte:head>
-
 <div class="flex h-1/3 flex-col items-center justify-around">
-	<div>
-		<h1 class="text-7xl font-bold text-slate-400">boltcutter.</h1>
-		<p class="text-center text-lg">because querystrings make links stupid.</p>
+	<div class="w-full text-center">
+		<h1 class="text-7xl font-bold text-sky-600">boltcutter.</h1>
+		<p class="text-lg">because querystrings make links look stupid.</p>
 	</div>
 
-	<form method="POST" use:enhance>
-		<label for="href" id="href" class="px-4 text-lg font-semibold">Enter a link to shorten</label>
+	<form method="POST" use:enhance class="flex flex-col items-center justify-center">
+		<label for="href" id="href" class="place-self-start px-4 text-lg font-semibold"
+			>Enter a link to shorten</label
+		>
 		<div class="flex">
 			<input
 				type="text"
@@ -45,16 +43,16 @@
 			/>
 			<button
 				type="submit"
-				class="rounded-r-2xl border border-blue-500 bg-blue-500 px-4 text-white transition-colors hover:border-blue-600 hover:bg-blue-600"
+				class="rounded-r-2xl border border-sky-600 bg-sky-600 px-4 text-white transition-colors hover:border-sky-700 hover:bg-sky-700"
 				>Shorten</button
 			>
 		</div>
 		{#if form}
 			<p class="pt-2 text-center text-sm text-green-500 text-shadow">
-				Success! Your link has been shortened. <button
+				Success! <button
 					onclick={copyToClipboard}
 					class="underline transition-colors hover:text-green-600"
-					>Click to copy to clipboard.</button
+					>Click to copy link to clipboard.</button
 				>
 			</p>
 		{/if}
